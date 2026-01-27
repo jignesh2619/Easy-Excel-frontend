070d47761219010395ae4dd6a15abfd10cf57945
jignesh2619 <136894974+jignesh2619@users.noreply.github.com>
2026-01-27 22:04:35 +0530

    Remove DashboardPreviewSection

--- src/App.tsx
+++ src/App.tsx
@@ -160,7 +160,6 @@ export default function App() {
         <BeforeAfterSection />
         <PromptToolSection />
         <FeaturesSection />
-        <DashboardPreviewSection />
         <ProductHuntSection />
         <PricingSection />
         <TokenDashboard />
